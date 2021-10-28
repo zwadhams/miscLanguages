a422b65c9f981dc8cead96b9d734d65bd14dec21
Zach Wadhams <33103466+zwadhams@users.noreply.github.com>
2021-10-28 11:04:49 -0700

    Added code and screenshot of run

---
+++ Prolog/hallway.pl
@@ -0,0 +1,18 @@
+%Zach Wadhams Prolog HW
+
+%Sets up the dorm structure
+dorm([room(_, 5), room(_, 4), room(_, 3), room(_, 2), room(_, 1)]).
+adjacent(X,Y) :- X =:= Y+1.
+adjacent(X,Y) :- X =:= Y-1.
+%creates the hallway
+hallway(X) :- dorm(X),
+	     member(room(hunter,A),X), A\=5,
+ 	     member(room(laura, B),X), B\=1,
+	     member(room(arnie, C),X), C\=5, C\=1,  
+             
+	     \+(adjacent(C,B)),
+	     
+	     member(room(ramey, D),X), D > B, 	     
+	     member(room(addiley, K),X),
+	     
+	     \+(adjacent(C,K)).
